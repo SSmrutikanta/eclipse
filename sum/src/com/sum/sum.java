147a3c4620f0936cafccb40e8ae3e89d7ff7dd87
smrutikanta <skswainswain4595@gmail.com>
2023-05-01 19:50:40 +0530

    initial commit

---
+++ sum/src/com/sum/sum.java
@@ -0,0 +1,11 @@
+package com.sum;
+
+public class sum {
+
+	public static void main(String[] args) {
+		int a=5;
+		int b=6;
+		System.out.println("the sum is"+  (a+b));
+		
+	}
+}
---
+++ sum/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author skswa
+ *
+ */
+module sum {
+}
